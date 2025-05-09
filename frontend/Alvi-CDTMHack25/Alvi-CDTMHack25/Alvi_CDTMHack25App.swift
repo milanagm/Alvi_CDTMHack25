1c3f86f2b35e2dedd94c2ee2b772fba941c27bba
Max Rosenblattl <max.rosenblattl@tum.de>
2025-05-10 01:18:57 +0200

    Setup Xcode project

---
+++ frontend/Alvi-CDTMHack25/Alvi-CDTMHack25/Alvi_CDTMHack25App.swift
@@ -0,0 +1,17 @@
+//
+//  Alvi_CDTMHack25App.swift
+//  Alvi-CDTMHack25
+//
+//  Created by Max Rosenblattl on 10.05.25.
+//
+
+import SwiftUI
+
+@main
+struct Alvi_CDTMHack25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/Alvi-CDTMHack25/Alvi-CDTMHack25/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alvi-CDTMHack25
+//
+//  Created by Max Rosenblattl on 10.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
